19bd760da515d0cda1327f48bb7da7a6130cd00e
KujoKnight <kujoknight@gmail.com>
2019-09-10 09:12:54 -0400

    The Game Loop

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -4,25 +4,43 @@ namespace Zork
 {
     class Program
     {
-        private static Commands ToCommand(string commandString)
-        {
-            if(Enum.TryParse<Commands>(commandString, true, out Commands result))
-            {
-                return result;
-            }
-            else
-            {
-                return Commands.UNKNOWN;
-            }
-        }
-
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKNOWN;
+            while(command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        outputString = $"You moved {command}.";
+                        break;
+
+                    default:
+                        outputString = "Unknown command";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+            }
         }
+
+        private static Commands ToCommand(string commandString) => (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
     }
 }
