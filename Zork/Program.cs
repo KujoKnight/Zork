df1db2d485ccd44dc6f0315bdabaa8f24aa89bc3
John <37167651+KujoKnight@users.noreply.github.com>
2019-09-11 14:36:24 -0400

    The Game World

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -4,19 +4,35 @@ namespace Zork
 {
     class Program
     {
-        private static string[] Rooms;
+        private static string outputString;
+        private static string[] Rooms = new string[] {"in the Forest", "West of the House", "behind the House", "at the clearing", "at the canyon"};
+        private static int x;
+        private static bool canMove;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Zork!");
+            canMove = true;
+            x = 1;
+            Console.Write("Welcome to Zork!");
 
             Commands command = Commands.UNKNOWN;
             while(command != Commands.QUIT)
             {
+                if(x >= 0 && x <= Rooms.Length && canMove == true)
+                {
+                    Console.CursorLeft = Console.BufferWidth - 32;
+                    Console.WriteLine("You are " + Rooms[x]);
+                }
+                else if(canMove == false)
+                {
+                    Console.CursorLeft = Console.BufferWidth - 32;
+                    Console.WriteLine("You are " + Rooms[x]);
+                    Console.CursorLeft = Console.BufferWidth - 32;
+                    Console.WriteLine("The way is blocked");
+                }
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
-                string outputString;
                 switch (command)
                 {
                     case Commands.QUIT:
@@ -31,7 +47,46 @@ namespace Zork
                     case Commands.SOUTH:
                     case Commands.EAST:
                     case Commands.WEST:
-                        outputString = $"You moved {command}.";
+                        if (Move(command) == true)
+                        {
+                            outputString = $"You moved {command}.";
+                        }
+
+                        switch (command)
+                        {
+                            case Commands.NORTH:
+                                //Go North
+                                break;
+                            case Commands.SOUTH:
+                                //Go South
+                                break;
+                            case Commands.EAST:
+                                //Go East
+                                if (x < Rooms.Length - 1)
+                                {
+                                    canMove = true;
+                                    x += 1;
+                                }
+                                else
+                                {
+                                    canMove = false;
+                                }
+                                break;
+                            case Commands.WEST:
+                                //Go West
+                                if (x > 0)
+                                {
+                                    canMove = true;
+                                    x -= 1;
+                                }
+                                else
+                                {
+                                    canMove = false;
+                                }
+                                break;
+                            default:
+                                break;
+                        }
                         break;
 
                     default:
@@ -44,5 +99,25 @@ namespace Zork
         }
 
         private static Commands ToCommand(string commandString) => (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
+
+        private static bool Move(Commands com)
+        {
+            try
+            {
+                if (com == Commands.NORTH || com == Commands.SOUTH || com == Commands.EAST || com == Commands.WEST)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+        }
     }
 }
