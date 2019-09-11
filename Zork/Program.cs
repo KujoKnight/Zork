9e93a53f328db114201ab6d9e77d44684cb94274
John <37167651+KujoKnight@users.noreply.github.com>
2019-09-11 15:21:35 -0400

    Updated Expanding the Frontier

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,146 +1,105 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace Zork
 {
-    class Program
+    internal class Program
     {
-        private static string outputString;
-        private static readonly string[,] Rooms = {
-            {"at the Rocky Trail", "South of the House", "at the Canyon View"},
-            {"at the Forest","West of the House","behind the House"},
-            {"in the Dense Woods","North of the House","at the Clearing"}
-        };
-        private static (int x, int y) Location;
-        private static bool canMove;
+        private static string currentRoom
+        {
+            get
+            {
+                return Rooms[Location.x, Location.y];
+            }
+        }
 
         static void Main(string[] args)
         {
-            Location.x = 1;
-            Location.y = 1;
-            canMove = true;
             Console.Write("Welcome to Zork!");
 
             Commands command = Commands.UNKNOWN;
             while(command != Commands.QUIT)
             {
-                if(Location.x >= 0 && Location.x <= Rooms.Length / 3 && Location.y >= 0 && Location.y <= Rooms.Length / 3 && canMove == true)
-                {
-                    Console.CursorLeft = Console.BufferWidth - 32;
-                    Console.WriteLine("You are " + Rooms[Location.x,Location.y]);
-                }
-                else if(canMove == false)
-                {
-                    Console.CursorLeft = Console.BufferWidth - 32;
-                    Console.WriteLine("You are " + Rooms[Location.x, Location.y]);
-                    Console.CursorLeft = Console.BufferWidth - 32;
-                    Console.WriteLine("The way is blocked");
-                }
+                Console.CursorLeft = Console.BufferWidth - 32;
+                Console.WriteLine("You are " + Rooms[Location.x, Location.y]);
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
                 switch (command)
                 {
                     case Commands.QUIT:
-                        outputString = "Thank you for playing!";
+                        Console.WriteLine("Thank you for playing!");
                         break;
 
                     case Commands.LOOK:
-                        outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.");
                         break;
 
                     case Commands.NORTH:
                     case Commands.SOUTH:
                     case Commands.EAST:
                     case Commands.WEST:
-                        if (Move(command) == true)
+                        if (Move(command) == false)
                         {
-                            outputString = $"You moved {command}.";
-                        }
-
-                        switch (command)
-                        {
-                            case Commands.NORTH:
-                                //Go North
-                                if (Location.x < Rooms.Length / 3 - 1)
-                                {
-                                    canMove = true;
-                                    Location.x += 1;
-                                }
-                                else
-                                {
-                                    canMove = false;
-                                }
-                                break;
-                            case Commands.SOUTH:
-                                //Go South
-                                if (Location.x > 0)
-                                {
-                                    canMove = true;
-                                    Location.x -= 1;
-                                }
-                                else
-                                {
-                                    canMove = false;
-                                }
-                                break;
-                            case Commands.EAST:
-                                //Go East
-                                if (Location.y < Rooms.Length / 3 - 1)
-                                {
-                                    canMove = true;
-                                    Location.y += 1;
-                                }
-                                else
-                                {
-                                    canMove = false;
-                                }
-                                break;
-                            case Commands.WEST:
-                                //Go West
-                                if (Location.y > 0)
-                                {
-                                    canMove = true;
-                                    Location.y -= 1;
-                                }
-                                else
-                                {
-                                    canMove = false;
-                                }
-                                break;
-                            default:
-                                break;
+                            Console.CursorLeft = Console.BufferWidth - 32;
+                            Console.WriteLine("The way is blocked.");
                         }
                         break;
 
                     default:
-                        outputString = "Unknown command";
+                        Console.WriteLine("Unknown command");
                         break;
                 }
-
-                Console.WriteLine(outputString);
             }
+        }
+
+        private static bool Move(Commands com)
+        {
+            Assert.IsTrue(canMove(com), "Invalid Direction");
+
+            bool validMovement = true;
+            switch (com)
+            {
+                case Commands.NORTH when Location.x > 0:
+                    Location.x--;
+                    break;
+                case Commands.SOUTH when Location.x > Rooms.GetLength(0) - 1:
+                    Location.x++;
+                    break;
+                case Commands.EAST when Location.y > Rooms.GetLength(1) - 1:
+                    Location.x++;
+                    break;
+                case Commands.WEST when Location.y > 0:
+                    Location.x--;
+                    break;
+
+                default:
+                    validMovement = false;
+                    break;
+            }
+
+            return validMovement;
         }
 
         private static Commands ToCommand(string commandString) => (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
 
-        private static bool Move(Commands com)
+        private static bool canMove(Commands com) => Directions.Contains(com);
+
+        private static readonly string[,] Rooms = {
+            {"at the Rocky Trail", "South of the House", "at the Canyon View"},
+            {"at the Forest","West of the House","behind the House"},
+            {"in the Dense Woods","North of the House","at the Clearing"}
+        };
+
+        private static readonly List<Commands> Directions = new List<Commands>
         {
-            try
-            {
-                if (com == Commands.NORTH || com == Commands.SOUTH || com == Commands.EAST || com == Commands.WEST)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            catch (Exception)
-            {
-                return false;
-                throw;
-            }
-        }
+            Commands.NORTH,
+            Commands.SOUTH,
+            Commands.EAST,
+            Commands.WEST
+        };
+
+        private static (int x, int y) Location = (1, 1);
     }
 }
