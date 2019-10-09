005830c96277cbfea0b77cc3c4ec1fb7448e4288
John <37167651+KujoKnight@users.noreply.github.com>
2019-10-09 14:23:56 -0400

    Zork Builder

---
+++ Zork.Builder.WinForms/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Zork.Builder.WinForms
+{
+    partial class Zork
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Zork
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 519);
+            this.Font = new System.Drawing.Font("Goudy Old Style", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Zork";
+            this.Text = "Zork";
+            this.Load += new System.EventHandler(this.Zork_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
